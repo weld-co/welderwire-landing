5a5838bdcfb88fc204d64660902c11d447db94ca
Peter Court <peter@petercourt.com>
2016-09-17 13:26:59 -0500

    updated styling

---
+++ tests/unit/dallas/route-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:dallas', 'Unit | Route | dallas', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
